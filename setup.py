4e5cb80b5b83c980d2568435372cd066b871c039
Francesco Vigni <vignif@gmail.com>
2022-09-16 12:21:26 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email='vignif@gmail.com',
     description='Description of my package',
     packages=find_packages(),
-    package_dir = {"": "."},
-    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1', 'sympy >= 1.1.1'],
+    package_dir={"": "."},
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1',
+                      'sympy >= 1.1.1', 'numpy-quaternion >= 2022.4.2'],
 )
