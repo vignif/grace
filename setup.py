4b7f876be2b1016ce71a58a682cdeb90f5847939
Francesco Vigni <vignif@gmail.com>
2022-09-16 12:22:40 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='grace',
     version='1.0.0',
-    url='https://github.com/mypackage.git',
-    author='Author Name',
+    url='https://github.com/vignif/grace.git',
+    author='Francesco Vigni',
     author_email='vignif@gmail.com',
-    description='Description of my package',
+    description='Geometric approach to mutual engagement',
     packages=find_packages(),
     package_dir={"": "."},
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1',
